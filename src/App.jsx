f984cd2e5a873a309d130dd2e0dae328327f5777
abhinav mulloli <abhinavmulloli1@gmail.com>
2024-10-26 07:47:36 +0530

    added header component and some style changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { FaSearch } from "react-icons/fa";
-import { BsCart3 } from "react-icons/bs";
-import { IoPerson } from "react-icons/io5";
-import Spinner from './Components/Spinner'; 
+import Spinner from './Components/Spinner';
 import ProductList from './Components/ProductList';
+import Header from './Components/Header';
 
 const App = () => {
   const [products, setProducts] = useState([]);
@@ -12,9 +10,8 @@ const App = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [loading, setLoading] = useState(true);
   const productsPerPage = 20;
-  const API_URL = 'https://dummyjson.com/products';
+  const API_URL = 'https://dummyjson.com/products';//api
 
- //fetching api
   const fetchProducts = async (page) => {
     setLoading(true);
     try {
@@ -32,7 +29,7 @@ const App = () => {
   useEffect(() => {
     fetchProducts(currentPage);
   }, [currentPage]);
- //search 
+
   const filteredProducts = products.filter(product =>
     product.title.toLowerCase().includes(searchQuery.toLowerCase())
   );
@@ -40,96 +37,14 @@ const App = () => {
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
-//menu 
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
 
   return (
-    // navbar
     <div>
-      <nav className="bg-black sticky top-0 z-10 shadow-md w-full">
-        <div className="container mx-auto flex justify-between items-center p-4">
-          <span className="text-white font-montserrat text-sm ps-1">
-            <b>No.1 Shopping App</b>
-          </span>
-          <span className="text-white font-montserrat ps-2 me-1 pe-1 flex items-center">
-            <IoPerson className="text-white me-3" /> <b>Login | Register</b>
-          </span>
-        </div>
-
-        <div className="bg-gray-100 shadow-md w-full md:px-20">
-          <div className="flex justify-between items-center p-6">
-            <span className="text-2xl font-bold text-green-500">OfficeDotCom</span>
-            <div className="hidden md:flex space-x-6 text-lg">
-              <a href="#" className="hover:text-orange-300">Home</a>
-              <a href="#" className="hover:text-orange-300">About Us</a>
-              <a href="#" className="hover:text-orange-300">Product</a>
-              <a href="#" className="hover:text-orange-300">Service & Support</a>
-              <a href="#" className="hover:text-orange-300">FAQ</a>
-              <a href="#" className="hover:text-orange-300">Contact Us</a>
-            </div>
-            <div className="hidden md:flex items-center space-x-4">
-              <input
-                type="text"
-                placeholder="Search..."
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-                className="border rounded px-2 py-1"
-              />
-              <button className="bg-transparent border rounded px-2 py-1">
-                <FaSearch />
-              </button>
-              <span className="ml-2">
-                <BsCart3 />
-              </span>
-            </div>
-            <div className="md:hidden">
-              <button onClick={toggleMenu} className="text-dark focus:outline-none">
-                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  {isMenuOpen ? (
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                  ) : (
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-                  )}
-                </svg>
-              </button>
-            </div>
-          </div>
-          {/* for menu bar in small screen */}
-          {isMenuOpen && (
-            <div className="md:hidden px-2 pt-2 pb-3 space-y-1">
-              <a href="#" className="block hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium">Home</a>
-              <a href="#" className="block hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium">About Us</a>
-              <a href="#" className="block hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium">Product</a>
-              <a href="#" className="block hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium">Service & Support</a>
-              <a href="#" className="block hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium">FAQ</a>
-              <a href="#" className="block hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium">Contact Us</a>
-              <a href="#" className="block hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-base font-medium">Log In</a>
-              <div className="flex items-center space-x-4">
-                <input
-                  type="text"
-                  placeholder="Search by product name..."
-                  value={searchQuery}
-                  onChange={(e) => setSearchQuery(e.target.value)}
-                  className="border rounded px-2 py-1"
-                />
-                <button className="bg-transparent border rounded px-2 py-1">
-                  <FaSearch />
-                </button>
-              </div>
-            </div>
-          )}
-        </div>
-      </nav>
-
+      <Header searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <main className="container mx-auto py-8">
         {loading ? (
-          //spinner
           <Spinner />
         ) : (
-          //product component
           <ProductList
             products={filteredProducts}
             onPageChange={handlePageChange}
--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -2,26 +2,28 @@ import React from 'react';
 
 const ProductList = ({ products, onPageChange, totalPages, currentPage }) => (
   <>
-    <div className="grid bg-slate-50 gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {Array.isArray(products) && products.length > 0 ? (
-        products.map((product) => (
-          <div
-            key={product.id}
-            className="card flex flex-col items-center p-4 bg-white shadow-lg hover:scale-105 transition-transform max-w-xs mx-4"
-          >
-            <img src={product.thumbnail} alt={product.title} className="h-40 w-3/4 object-cover mb-4" />
-            <h6 className="text-lg font-semibold text-gray-800 mt-2">{product.title}</h6>
-            <p className="text-gray-600">Price: ${product.price}</p>
-            <p className="text-gray-600">Rating: {product.rating}</p>
-            <button className="mt-4 bg-green-500 text-white py-2 px-4 rounded">
-              ENQUIRE
-            </button>
-          </div>
-        ))
-      ) : (
-        <p className="text-center text-gray-500">No products available.</p>
-      )}
-    </div>
+  {/* Product card */}
+<div className="grid bg-slate-50 gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-0 md:px-4 lg:px-6">
+  {Array.isArray(products) && products.length > 0 ? (
+    products.map((product) => (
+      <div
+        key={product.id}
+        className="card flex flex-col items-center p-4 bg-white shadow-lg hover:scale-105 transition-transform w-full"
+      >
+        <img src={product.thumbnail} alt={product.title} className="h-40 w-3/4 object-cover mb-4" />
+        <h6 className="text-lg font-semibold text-gray-800 mt-2">{product.title}</h6>
+        <p className="text-gray-600">Price: ${product.price}</p>
+        <p className="text-gray-600">Rating: {product.rating}</p>
+        <button className="mt-4 bg-green-500 text-white py-2 px-4 rounded">
+          ENQUIRE
+        </button>
+      </div>
+    ))
+  ) : (
+    <p className="text-center text-gray-500">No products available.</p>
+  )}
+</div>
+
 
     {/* Pagination Controls */}
     <div className="flex flex-wrap justify-center mt-8">
@@ -29,7 +31,8 @@ const ProductList = ({ products, onPageChange, totalPages, currentPage }) => (
         <button
           key={index + 1}
           onClick={() => onPageChange(index + 1)}
-          className={`w-1/5 md:w-auto px-3 py-1 mx-1 my-1 text-sm md:text-base ${currentPage === index + 1 ? 'bg-black text-white' : 'bg-gray-200 text-gray-800 hover:bg-gray-300'} rounded`}
+          className={`w-1/5 md:w-auto px-3 py-1 mx-1 my-1 text-sm md:text-base
+           ${currentPage === index + 1 ? 'bg-black text-white' : 'bg-gray-200 text-gray-800 hover:bg-gray-300'} rounded`}
         >
           {index + 1}
         </button>
